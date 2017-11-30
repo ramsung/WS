9b9ebd31752e0c2e3ce4b48bbe8cabf5e682d551
mohan <immohanravi@gmail.com>
2017-11-30 21:10:47 +0530

    download

--- app/src/main/java/com/beyonity/whatsappsharing/MainActivity.java
+++ app/src/main/java/com/beyonity/whatsappsharing/MainActivity.java
@@ -1,13 +1,169 @@
 package com.beyonity.whatsappsharing;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "main";
+    ImageView play;
 
+    public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
+    private Button startBtn;
+    private ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isStoragePermissionGranted();
+        startDownload();
+        //String uri = "https://beyonitysoftwares.cf:2083/cpsess7496955280/download?skipencode=1&file=%2fhome%2fbeyonity%2fpublic_html%2fws%2fRaja%20Ko%20Rani%20Se%20Pyaar%20Ho%20Gaya-u8iT.mp4";
+
+//        String u = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+        /*play = (ImageView) findViewById(R.id.imageView);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+
+                    VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+                    MediaController mediaController = new MediaController(MainActivity.this);
+                    mediaController.setAnchorView(videoView);
+                    Uri video = Uri.parse(u);
+                    videoView.setMediaController(mediaController);
+                    videoView.setVideoURI(video);
+                    // Close the progress bar and play the video
+                    videoView.setOnPreparedListener(mp -> videoView.start());
+
+
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    Toast.makeText(MainActivity.this, "error connecting", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });*/
     }
+
+    private void startDownload() {
+        String url = "https://beyonitysoftwares.cf:2083/cpsess0250457168/viewer/home%2fbeyonity%2fpublic_html%2fws/Raja%20Ko%20Rani%20Se%20Pyaar%20Ho%20Gaya-u8iT.mp4";
+        new DownloadFileAsync().execute(url);
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DIALOG_DOWNLOAD_PROGRESS:
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Downloading file..");
+                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                mProgressDialog.setCancelable(false);
+                mProgressDialog.show();
+                return mProgressDialog;
+            default:
+                return null;
+        }
+    }
+    class DownloadFileAsync extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(DIALOG_DOWNLOAD_PROGRESS);
+        }
+
+        @Override
+        protected String doInBackground(String... aurl) {
+            int count;
+
+            try {
+                URL url = new URL(aurl[0]);
+                URLConnection conexion = url.openConnection();
+                conexion.connect();
+
+                int lenghtOfFile = conexion.getContentLength();
+                Log.d("ANDRO_ASYNC", "Lenght of file: " + lenghtOfFile);
+
+                InputStream input = new BufferedInputStream(url.openStream());
+                OutputStream output = new FileOutputStream("/sdcard/Download/test.mp4");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress(""+(int)((total*100)/lenghtOfFile));
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+            } catch (Exception e) {}
+            return null;
+
+        }
+        protected void onProgressUpdate(String... progress) {
+            Log.d("ANDRO_ASYNC",progress[0]);
+            mProgressDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        @Override
+        protected void onPostExecute(String unused) {
+            dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
+        }
+    }
+
+    public  boolean isStoragePermissionGranted() {
+
+            if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
+    }
+
 }
+
+
